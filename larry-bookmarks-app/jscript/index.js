743c5c80f30922cfd7da940561c0ca334215c55d
larry <larryzie@gmail.com>
2019-09-29 17:37:15 -0500

    Started adding js for the render function

--- larry-bookmarks-app/jscript/index.js
+++ larry-bookmarks-app/jscript/index.js
@@ -24,4 +24,61 @@ const store = {
   filter: 0
 };
 
+function render(myScreen) {
+  switch(myScreen) {
+  case 'main': {
+    let htmlString = generateMainString();
+    $('.js-mainWindow').html(htmlString);    
+  }
+    break;
+
+  }  
+
+}
+
+function generateMainString() {
+  return `<section class="upperContainer">
+            <div class="newBookmark">
+              <label for="buttonNB">New Bookmark:</label><br>
+              <button class="buttonNew" name="buttonNB" type="button">+ New Bookmark</button>
+            </div>
+            <div class="filterBy">
+              <label for="filter">Filter By: </label><br>          
+              <select id="js-filter" name="filter">
+                <option value="" selected="selected">Filter</option>            
+                <option value="oneStar">One Star</option>
+                <option value="twoStar">Two Star</option>
+                <option value="threeStar">Three Star</option>
+                <option value="fourStar">Four Star</option>
+                <option value="fiveStar">Five Star</option>                                                
+              </select>
+            </div> -->
+          </section>
+          <section class="bookmarks">
+            <ul>
+              <li>Bookmark#1</li>
+              <li>Bookmark#2</li>
+              <li>Bookmark#3</li>
+              <li>Bookmark#4</li>
+              <li>Bookmark#5</li>                                
+            </ul>
+          </section>`;
+}
+
+function initialize() {
+  render('main');
+}
+
+function handleAddBookmark() {
+  $('.buttonNew').on('click', function (event) {
+    console.log(`Click Button Add Bookmark, ${JSON.stringify(event)}`);
+    render('add');
+  });
+}
+
+function main() {
+  initialize();
+  handleAddBookmark();
+}
+
 main ();
